f2eb92b695ce1f1121ffbe2742cae9648d508500
muhamad gatot <gatotbima2002@gmail.com>
2025-09-09 19:32:28 +0700

    fix: vercel.json

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import serverless from "serverless-http";
-import { buildApp } from "../src/app";
-
-const app = buildApp();
-
-export default serverless(app);   // ✅ export handler, don’t call listen()
